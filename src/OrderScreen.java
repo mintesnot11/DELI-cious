88049209cb7e7c2d248b69791dc0a9e43fb57be1
mintesnot11 <mintesnotgirma@icloud.com>
2025-05-22 23:34:36 -0700

    Added Add Sandwich Screen

--- src/OrderScreen.java
+++ src/OrderScreen.java
@@ -1,3 +1,4 @@
+// src/OrderScreen.java
 import java.util.Scanner;
 
 public class OrderScreen {
@@ -10,6 +11,43 @@ public class OrderScreen {
     }
 
     public void display() {
-        System.out.println("\n(Order screen coming soon...)\n");
+        boolean ordering = true;
+
+        while (ordering) {
+            System.out.println("\n--- ORDER MENU ---");
+            System.out.println("1) Add Sandwich");
+            System.out.println("2) Add Drink");
+            System.out.println("3) Add Chips");
+            System.out.println("4) Checkout");
+            System.out.println("0) Cancel Order");
+            System.out.print("Choose an option: ");
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    AddSandwichScreen sandwichScreen = new AddSandwichScreen(scanner, order);
+                    sandwichScreen.display();
+                    break;
+                case "2":
+                    AddDrinkScreen drinkScreen = new AddDrinkScreen(scanner, order);
+                    drinkScreen.display();
+                    break;
+                case "3":
+                    AddChipsScreen chipsScreen = new AddChipsScreen(scanner, order);
+                    chipsScreen.display();
+                    break;
+                case "4":
+                    CheckoutScreen checkoutScreen = new CheckoutScreen(scanner, order);
+                    checkoutScreen.display();
+                    ordering = false;
+                    break;
+                case "0":
+                    System.out.println("Order cancelled. Returning to Home Screen.");
+                    ordering = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 }
