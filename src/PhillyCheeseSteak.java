cff9b057fa7e7025c2fcee7b889a02823551ad74
mintesnot11 <mintesnotgirma@icloud.com>
2025-05-28 01:25:18 -0700

    Added Philly Cheese Steak Signature Sandwich

---
+++ src/PhillyCheeseSteak.java
@@ -0,0 +1,13 @@
+public class PhillyCheeseSteak extends SignatureSandwich {
+    public PhillyCheeseSteak() {
+        super(8, "white", true, 25);
+    }
+
+    @Override
+    protected void buildSignature() {
+        addMeat(new Meat("steak", 5, 0));
+        addCheese(new Cheese("american", 2,0));
+        addTopping(new RegularTopping("peppers"));
+        addTopping(new RegularTopping("mayo"));
+    }
+}
